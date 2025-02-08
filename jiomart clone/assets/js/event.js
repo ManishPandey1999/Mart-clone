37014aa283ad54f79836406b17d14638afb66fd3
ManishPandey1999 <160839118+ManishPandey1999@users.noreply.github.com>
2025-02-08 17:48:40 +0530

    Delete jiomart clone/assets directory

--- jiomart clone/assets/js/event.js
+++
@@ -1,9 +0,0 @@
-// Moving slider on main page
-
-const slider = document.querySelectorAll('.slide');
-const nextSlidebutton = document.querySelector('#nextSlidebtn');
-const prevSlidebutton = document.querySelector('#prevSlidebtn');
-
-nextSlidebutton.addEventListener("click", () => {
-    
-})
